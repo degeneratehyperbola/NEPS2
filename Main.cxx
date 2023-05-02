3f98342c28d0a2654c68a0b069c2218e5fe22640
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2023-05-02 22:35:42 +0200

    Update initialization code

--- Main.cxx
+++ Main.cxx
@@ -4,33 +4,56 @@
 #include <imgui/imgui_impl_dx11.h>
 
 #include "Globals.hxx"
-#include "MemorySearch.hxx"
+#include "MemoryTools.hxx"
 #include "HookCallbacks.hxx"
 
 // DLL entry point
-BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD ulReasonForCall, LPVOID lpReserved)
 {
-	switch (ul_reason_for_call)
+	switch (ulReasonForCall)
 	{
 	case DLL_PROCESS_ATTACH:
-		Hooks::g_hModule = hModule;
-		// Memory::g_pPresent = MemorySearch::FindPattern();
+		// Initialize globals
+		g_hModule = hModule;
+		g_hWnd = FindWindowW(L"Valve001", nullptr);
+		g_pSwapChain = MemorySearch::RelativeToAbsolute<decltype(g_pSwapChain)>(MemorySearch::FindPattern("rendersystemdx11.dll", "66 0F 7F 05 ? ? ? ? 66 0F 7F 0D ? ? ? ? 48 89 35 ? ? ? ?"));
 
+		// Setup ImGui
+		ImGui::CreateContext();
+
+		ID3D11Device* pDevice = nullptr;
+		ID3D11DeviceContext* pDeviceContext = nullptr;
+
+		g_pSwapChain->GetDevice(__uuidof(ID3D11Device), reinterpret_cast<void**>(&pDevice));
+		pDevice->GetImmediateContext(&pDeviceContext);
+		
+		ImGui_ImplDX11_Init(pDevice, pDeviceContext);
+
+		// Setup our hooking method		
 		if (!HookManager::Setup())
 			return FALSE;
 
-		ImGui::CreateContext();
+		// Hook interesting functions
+		auto pSwapChainVtable = GET_VTABLE(g_pSwapChain);
+		g_hmPresent = HookManager(pSwapChainVtable[8], Callbacks::Present);
+		g_hmResizeBuffers = HookManager(pSwapChainVtable[13], Callbacks::ResizeBuffers);
 
-		Hooks::g_hmPresentHook = HookManager(Memory::g_pPresent, PresentCallback);
-
-		Hooks::g_hmPresentHook.Hook();
+		g_hmPresent.Hook();
+		g_hmResizeBuffers.Hook();
 
 		break;
 	case DLL_PROCESS_DETACH:
-		Hooks::g_hmPresentHook.Unhook();
+		// Unhook hooked hooks
+		g_hmResizeBuffers.Unhook();
+		g_hmPresent.Unhook();
+		
+		// Cleanup after ourselves
+		HookManager::Cleanup();
+
+		// Cleanup ImGui
 		ImGui_ImplDX11_Shutdown();
 		ImGui::DestroyContext();
-		HookManager::Cleanup();
+		
 		break;
 	}
 	return TRUE;
