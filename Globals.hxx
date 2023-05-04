61464d944b8d3d325bb9ae94441df77f3e9de33d
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2023-05-05 01:30:56 +0200

    Grammar in code comments

--- Globals.hxx
+++ Globals.hxx
@@ -10,6 +10,6 @@ extern HWND g_hWnd; // General globals | Handle to the game's window
 extern bool g_bIsUsingVulkan; // Rendering related globals
 extern ID3D11Device* g_pD3DDevice; // Rendering related globals | Hijacked D3D device interface (reference count not incremented)
 
-extern WNDPROC g_pOriginalWndProc; // Hook related globals | Callbacks::WndProc
-extern HookManager g_hkPresent; // Hook related globals | Callbacks::Present
-extern HookManager g_hkResizeBuffers; // Hook related globals | Callbacks::ResizeBuffers
+extern WNDPROC g_pOriginalWndProc; // Hooking related globals | Callbacks::WndProc
+extern HookManager g_hkPresent; // Hooking related globals | Callbacks::Present
+extern HookManager g_hkResizeBuffers; // Hooking related globals | Callbacks::ResizeBuffers
