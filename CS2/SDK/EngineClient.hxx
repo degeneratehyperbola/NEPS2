11554f2f6b771752d46ff5663481d671a62ffeef
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2023-05-15 02:29:33 +0200

    Add `EngineClient` interface

---
+++ CS2/SDK/EngineClient.hxx
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <MemoryTools.hxx>
+
+#define ENGINE_CLIENT_VERSION "Source2EngineToClient001"
+
+class IEngineClient
+{
+public:
+	VIRTUAL_METHOD(30, bool, IsInGame, (), (this))
+	VIRTUAL_METHOD(31, bool, IsConnected, (), (this))
+	VIRTUAL_METHOD(48, void, GetScreenSize, (int& width, int& height), (this, std::ref(width), std::ref(height)))
+	VIRTUAL_METHOD(51, const char*, GetLevelName, (), (this))
+	VIRTUAL_METHOD(52, const char*, GetLevelNameShort, (), (this))
+};
+
