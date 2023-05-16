51da5641efc7956534660205ca055352f6b2cdb5
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2023-05-17 01:10:52 +0200

    Make globals inline instead of external references

--- Globals.hxx
+++ Globals.hxx
@@ -1,15 +1,13 @@
 #pragma once
 
-#include <Windows.h>
-#include <d3d11.h>
-#include <HookManager.hxx>
+#include <PCH.h>
 
-extern HMODULE g_hModule; // General globals | Handle to this DLL
-extern HWND g_hWnd; // General globals | Handle to the game's window
+inline HMODULE g_hModule; // General globals | Handle to this DLL
+inline HWND g_hWnd; // General globals | Handle to the game's window
 
-extern bool g_bIsUsingVulkan; // Rendering related globals
-extern bool g_bImGuiInitialized; // Rendering related globals | Is ImGui backend initialized
+inline bool g_bIsUsingVulkan; // Rendering related globals
+inline bool g_bImGuiInitialized; // Rendering related globals | Is ImGui backend initialized
 
-extern WNDPROC g_pOriginalWndProc; // Hooking related globals | Callbacks::WndProc
-extern HookManager g_hkPresent; // Hooking related globals | Callbacks::Present
-extern HookManager g_hkResizeBuffers; // Hooking related globals | Callbacks::ResizeBuffers
+inline WNDPROC g_pOriginalWndProc; // Hooking related globals | Callbacks::WndProc
+inline HookManager g_hkPresent; // Hooking related globals | Callbacks::Present
+inline HookManager g_hkResizeBuffers; // Hooking related globals | Callbacks::ResizeBuffers
