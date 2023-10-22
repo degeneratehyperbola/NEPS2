40a0a5e96f4fbc11678bf75786c76fbaf3800fc4
degeneratehyperbola <65950527+degeneratehyperbola@users.noreply.github.com>
2023-10-22 22:37:54 +0200

    style: Add comments for SDL methods

--- CS2/General.cxx
+++ CS2/General.cxx
@@ -1,24 +1,28 @@
 #include "General.hxx"
 
 
+// Enables or disables relative mouse mode, where the mouse stays in one place but sends move events
 void CS2::SetRelativeMouseMode(bool enabled)
 {
 	static auto fn = EXTERNAL_PROC("SDL3", "SDL_SetRelativeMouseMode", int, (bool));
 	if (fn) fn(enabled);
 }
 
+// Enables or disables grabbing of mouse events
 void CS2::SetMouseCapture(bool enabled)
 {
 	static auto fn = EXTERNAL_PROC("SDL3", "SDL_SetWindowMouseGrab", void, (void*, bool));
 	if (fn) fn(InputSystem->GetSDLWindow(), enabled);
 }
 
+// Sets mouse position inside the client area
 void CS2::WarpMouseInWindow(int x, int y)
 {
 	static auto fn = EXTERNAL_PROC("SDL3", "SDL_WarpMouseInWindow", void, (void*, int, int));
 	if (fn) fn(InputSystem->GetSDLWindow(), x, y);
 }
 
+// Gets width and height of the client area
 void CS2::GetWindowSize(int* w, int* h)
 {
 	static auto fn = EXTERNAL_PROC("SDL3", "SDL_GetWindowSize", int, (void*, int*, int*));
